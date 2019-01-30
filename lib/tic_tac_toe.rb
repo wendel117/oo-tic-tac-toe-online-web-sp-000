9dedeee5a7ebe8547fb0a686e713d253564159e5
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-30 22:49:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,7 +21,7 @@ def display_board
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def move(board, index, current_player)
+  def move(index, current_player)
   @board[index] = current_player
 end
 
