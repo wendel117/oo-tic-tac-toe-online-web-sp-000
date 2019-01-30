dedc57540a58326891764a74186f0b8db34fb2f0
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-30 22:48:05 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,6 +13,14 @@ WIN_COMBINATIONS = [
   [0, 4, 8],
   [6, 4, 2]
 ]
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
 
 def input_to_index(user_input)
   user_input.to_i - 1
